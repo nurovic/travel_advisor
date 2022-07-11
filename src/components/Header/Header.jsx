141a57db506862c91e28d72f5e0094616516662e
nurovic <mertnurovic@gmail.com>
2022-07-11 14:12:08 +0300

    Dynamic Search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,23 @@
+import React,{ useState } from "react"
 import { Autocomplete } from "@react-google-maps/api"
 import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
 
 import useStyle from './styles'
 
-const Header = () => {
+const Header = ({setCoordinates}) => {
   const classes = useStyle()
 
+  const [autocomplate, setAutoComplate] = useState(null)
+  
+  const onLoad = (autoC) => setAutoComplate(autoC)
+
+  const onPlaceChanged = () => {
+    const lat = autocomplate.getPlace().geometry.location.lat()
+    const lng = autocomplate.getPlace().geometry.location.lng()
+
+    setCoordinates({lat, lng})
+  }
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
@@ -17,14 +28,14 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          {/* <Autocomplete> */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder='Search... ' classes={{root: classes.inputRoot, input: classes.inputInput }}/>
             </div>
-          {/* </Autocomplete> */}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
